86eba17750267f001aea48e5f425caaac70462a4
hiroto <o.hyshrt.19@gmail.com>
2022-02-22 19:43:44 +0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// const target = document.getElementById("menu");
-// const drawer = document.getElementById("sp-nav");
-// target.addEventListener('click', () => {
-//   target.classList.toggle('open');
-//   drawer.classList.toggle('in');
-// });
-
-// function hamburger() {
-//   document.getElementById('line1').classList.toggle('line_1');
-//   document.getElementById('line2').classList.toggle('line_2');
-//   document.getElementById('line3').classList.toggle('line_3');
-//   document.getElementById('nav').classList.toggle('in');
-// }
-// document.getElementById('hamburger').addEventListener('click' , function () {
-//   hamburger();
-// } );
-
 const target = document.getElementById("menu");
 
 target.addEventListener('click', () => {
